e7f378491de6d03d02e35f8df97908e6256929ad
Hushen Savani <hushen.savani@azilen.com>
2023-05-04 12:28:53 +0530

    first commit

---
+++ src/main/java/com/mfsa/adapter/AdapterCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.mfsa.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdapterCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfsa/adapter/AdapterCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfsa.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
