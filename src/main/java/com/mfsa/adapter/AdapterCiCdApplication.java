250800845b53392fb80733c629f106b7d1084118
Hushen Savani <hushen.savani@azilen.com>
2023-05-04 13:09:25 +0530

    -- controller added

--- src/main/java/com/mfsa/adapter/AdapterCiCdApplication.java
+++ src/main/java/com/mfsa/adapter/AdapterCiCdApplication.java
@@ -10,7 +10,6 @@ public class AdapterCiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdapterCiCdApplication.class, args);
-		System.out.println("App started");
 	}
 
 	@Bean
